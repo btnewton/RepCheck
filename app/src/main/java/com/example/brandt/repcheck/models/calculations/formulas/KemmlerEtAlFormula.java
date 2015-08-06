9a6c4cfbf65b2ba4ba986511541b3cc6fb562d08
unknown <bnewton149@gmail.com>
2015-08-05 22:15:45 -0400

    Fixed Kemmler

--- app/src/main/java/com/example/brandt/repcheck/models/calculations/formulas/KemmlerEtAlFormula.java
+++ app/src/main/java/com/example/brandt/repcheck/models/calculations/formulas/KemmlerEtAlFormula.java
@@ -2,19 +2,18 @@ package com.example.brandt.repcheck.models.calculations.formulas;
 
 /**
  * Kemmler et al
+ * source: http://www.unm.edu/~rrobergs/478PredictionAccuracy.pdf
  * Created by Brandt on 8/2/2015.
  */
 public class KemmlerEtAlFormula extends OneRepMaxFormula {
     @Override
     protected double calculateMax(int reps, double weight) {
-        return weight * (0.988 - (0.0000584 * Math.pow(reps, 3)
-                + 0.00190 * Math.pow(reps, 2) + 0.0104 * reps));
+        return weight * (0.988  + 0.0104 * reps  + 0.0019 * Math.pow(reps, 2)- 0.0000584 * Math.pow(reps, 3));
     }
 
     @Override
     protected double calculateWeightForRep(int reps) {
-        return max / (0.988 - (0.0000584 * Math.pow(reps, 3)
-                + 0.00190 * Math.pow(reps, 2) + 0.0104 * reps));
+        return max / (0.988  + 0.0104 * reps  + 0.0019 * Math.pow(reps, 2)- 0.0000584 * Math.pow(reps, 3));
     }
 
     @Override
